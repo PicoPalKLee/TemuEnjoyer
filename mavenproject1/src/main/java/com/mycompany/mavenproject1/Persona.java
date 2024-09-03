1434b5d994f5948e54d5ffed2ed2ab5f44cb66f4
Francisco Rivera} <friverad@correo.uss.cl>
2024-09-03 18:44:27 -0400

    Creacion de persona

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Persona.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Lab409
+ */
+public class Persona {
+
+    /**
+     * @return the rut
+     */
+    public String getRut() {
+        return rut;
+    }
+
+    /**
+     * @param rut the rut to set
+     */
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    private String rut;
+    private String Nombre;
+    private int Edad;
+    public Persona(){
+        
+    }
+    
+    
+}
+
+
